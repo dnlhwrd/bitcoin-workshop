964ed82f6de7a156a14c6098a60a45b2c605dea4
dnlhwrd <52698693+dnlhwrd@users.noreply.github.com>
2019-09-19 07:16:12 -0500

    Php
    Echo

--- practice-1/example-1.php
+++ practice-1/example-1.php
@@ -2,6 +2,5 @@
 
 // Для начала попробуем получить список всех возможных методов. Это делается с помощью метода help
 
-require_once('connect.php');
-
-echo daemonQuery('help');
+require_once('connect.php');'connect.php'
+echo daemonQuery('help');'help'
